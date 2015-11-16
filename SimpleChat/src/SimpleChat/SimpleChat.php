acf9ab5bfbc2b8b4ade437d2f0880cd8ba250aa9
Chen Zheng <czheng1968@gmail.com>
2015-11-15 20:58:46 -0500

    Update SimpleChat.php
    init

--- SimpleChat/src/SimpleChat/SimpleChat.php
+++ SimpleChat/src/SimpleChat/SimpleChat.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace SimpleChat;
+
+use pocketmine\plugin\PluginBase;
+
+class SimpleChat extends PluginBase implements Listener {
+  public function onEnable(){
+    if (!is_dir($this->getDataFolder())){
+      mkdir($this->getDataFolder());
+      //MAKES THE SETTING.JSON
+      $settings = array();
+      $words = array();
+      $settings["filterlevel"] = 2;
+      $settings["words"] = $words;
+      $settings["exclusionlist"] = "off";
+      $encoded = json_encode($settings);
+      $handle = fopen($this->getDataFolder."/settings.json", "w+");
+      fwrite($handle, $encoded);
+      fclose($handle);
+      
+    }
+  }
+}
