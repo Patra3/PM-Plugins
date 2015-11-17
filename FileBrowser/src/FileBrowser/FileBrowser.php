9e9fedf5f490267c4fdeeede540d15415d724a4d
Chen Zheng <czheng1968@gmail.com>
2015-11-17 08:50:52 -0500

    Update FileBrowser.php

--- FileBrowser/src/FileBrowser/FileBrowser.php
+++ FileBrowser/src/FileBrowser/FileBrowser.php
@@ -7,5 +7,10 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
 class FileBrowser extends PluginBase {
-  
+  public function onEnable(){
+    if (!is_dir($this->getDataFolder())){
+      mkdir($this->getDataFolder());
+      $this->saveDefaultConfig();
+    }
+  }
 }
