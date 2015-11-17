5875a70111820c34fd233b951e5353d75dcb6cca
Chen Zheng <czheng1968@gmail.com>
2015-11-17 08:49:57 -0500

    Update FileBrowser.php

--- FileBrowser/src/FileBrowser/FileBrowser.php
+++ FileBrowser/src/FileBrowser/FileBrowser.php
@@ -1,1 +1,11 @@
+<?php
 
+namespace FileBrowser;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class FileBrowser extends PluginBase {
+  
+}
