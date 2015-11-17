66e91a8670c5ac7cb1e8b50e9610161cd1d1db06
Chen Zheng <czheng1968@gmail.com>
2015-11-17 08:32:00 -0500

    Update AchievementPlus.php

--- AchievementPlus/src/AchievementPlus/AchievementPlus.php
+++ AchievementPlus/src/AchievementPlus/AchievementPlus.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace AchievementPlus;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class AchievementPlus extends PluginBase implements Listener {
+  public function onEnable(){
+    if (!is_dir($this->getDataFolder())){
+      mkdir($this->getDataFolder());
+      $this->saveDefaultConfig();
+    }
+    $data = array();
+  }
+}
