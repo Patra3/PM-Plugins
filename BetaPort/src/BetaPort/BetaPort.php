b9b744a8e93a1c97523b426889769269c32135a0
Chen Zheng <czheng1968@gmail.com>
2015-11-19 21:09:31 -0500

    Update BetaPort.php
    try #2

--- BetaPort/src/BetaPort/BetaPort.php
+++ BetaPort/src/BetaPort/BetaPort.php
@@ -3,17 +3,28 @@
 namespace BetaPort;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine;
+use pocketmine\event\server\QueryRegenerateEvent;
 use pocketmine\Server;
 
-class BetaPort extends PluginBase{
+//class Process extends QueryRegenerateEvent{
+  //$
+//}
+
+class BetaPort extends QueryRegenerateEvent{
   public function onEnable(){
-    $yy = new Server();
+    $server = $this->getServer();
+    $yy = new QueryRegenerateEvent(Server $server, $timeout = 5);
+    /*
     runkit_method_redefine(
       'Server',
       'getVersion',
       '',
-      'return "v0.13.0 alpha";';
-    );
+      'return "v0.13.0 alpha";',
+      RUNKIT_ACC_PUBLIC
+    );*/
+    $yy->version = "v0.13.0 alpha";
+    
     $this->getLogger()->info("BetaPort was successful!");
   }
 }
