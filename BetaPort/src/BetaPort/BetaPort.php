f096a305b7046eb31aa21c9b8b4755b509eadc8a
Chen Zheng <czheng1968@gmail.com>
2015-11-19 20:47:32 -0500

    hack
    hack

--- BetaPort/src/BetaPort/BetaPort.php
+++ BetaPort/src/BetaPort/BetaPort.php
@@ -1,1 +1,19 @@
+<?php
 
+namespace BetaPort;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+
+class BetaPort extends PluginBase{
+  public function onEnable(){
+    $yy = new Server();
+    runkit_method_redefine(
+      'Server',
+      'getVersion',
+      '',
+      'return "v0.13.0 alpha";';
+    );
+    $this->getLogger()->info("BetaPort was successful!");
+  }
+}
