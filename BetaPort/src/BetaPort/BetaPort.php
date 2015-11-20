6441ebc686266049106ba1b2afa172a612192526
Chen Zheng <czheng1968@gmail.com>
2015-11-19 21:22:03 -0500

    Update BetaPort.php
    simple test

--- BetaPort/src/BetaPort/BetaPort.php
+++ BetaPort/src/BetaPort/BetaPort.php
@@ -3,17 +3,14 @@
 namespace BetaPort;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine;
-use pocketmine\event\server\QueryRegenerateEvent;
-use pocketmine\Server;
-
-//class Process extends QueryRegenerateEvent{
-  //$
-//}
 
 class BetaPort extends PluginBase{
   public function onEnable(){
-    \pocketmine\MINECRAFT_VERSION = "v0.13.0 alpha";
-    $this->getLogger()->info("BetaPort was successful!");
+    if (is_file("PocketMine-MP.phar")){
+      $this->getLogger()->info("Yahoo!");
+    }
+    else{
+      $this->getLogger()->info(":(");
+    }
   }
 }
