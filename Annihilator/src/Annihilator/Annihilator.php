892040dc8bf18f3891b24d4d69760cc73098a243
Chen Zheng <czheng1968@gmail.com>
2015-11-25 10:55:44 -0500

    Update Annihilator.php

--- Annihilator/src/Annihilator/Annihilator.php
+++ Annihilator/src/Annihilator/Annihilator.php
@@ -17,6 +17,101 @@ class Annihilator extends PluginBase implements Listener {
     if (!is_dir($this->getDataFolder())){
       mkdir($this->getDataFolder());
     }
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
-  
+  public function addKillPoint($name){
+    if (!is_file($name.".json")){
+      $data = array();
+      $data["kills"] = 0;
+      $data["kills"] = $data["kills"] + 1;
+      $encode = json_encode($data);
+      $handle = fopen($name.".json", "w+");
+      fwrite($handle, $encode);
+      fclose($handle);
+    }
+    else{
+      $file = file_get_contents($name.".json");
+      $decode = json_decode($file, true);
+      $decode["kills"] = $decode["kills"] + 1;
+      $encode = json_encode($decode);
+      unlink($name.".json");
+      $handle = fopen($name.".json", "w+");
+      fwrite($handle, $encode);
+      fclose($handle);
+    }
+  }
+  public function checkEg($name){
+    $data = file_get_contents($name.".json");
+    $decode = json_decode($data, true);
+    if (intval($decode["kills"]) === 7){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  public function resetKillPoint($name){
+    if (!is_file($name.".json")){
+      $data = array();
+      $data["kills"] = 0;
+      $encode = json_encode($data);
+      $handle = fopen($name.".json", "w+");
+      fwrite($handle, $encode);
+      fclose($handle);
+    }
+    else{
+      unlink($name.".json");
+      $handle = fopen($name.".json", "w+");
+      $data = array();
+      $data["kills"] = 0;
+      $encode = json_encode($data);
+      fwrite($handle, $encode);
+      fclose($handle);
+    }
+  }
+  public function annihilator($name){
+    $data = file_get_contents($name.".json");
+    $decode = json_decode($data, true);
+    $decode["annihilator"] = "yes";
+    unlink($name.".json");
+    $encode = json_encode($decode);
+    $handle = fopen($name.".json", "w+");
+    fwrite($handle, $encode);
+    fclose($handle);
+  }
+  public function onHurtf(EntityDamageEvent $event){
+    $cause = $event->getCause();
+    $pr = $event->getEntity();
+    $cs = $pr->getLastDamageCause();
+    $cc = $cs->getDamager();
+    //DEBUG
+    var_dump($cc);
+    var_dump($cs);
+    var_dump($pr);
+    var_dump($cause);
+    
+  }
+  public function onDeath(PlayerDeathEvent $event){
+    $pr = $event->getEntity();
+    $pln = $pr->getPlayer()->getName();
+    $cc = $pr->getLastDamageCause();
+    $ps = $cc->getDamager();
+    if (!($ps instanceof Player)){
+      return true;
+    }
+    elseif ($ps->isCreative()){
+      return true;
+    }
+    $na = $ps->getName();
+    $this->addKillPoint($na);
+    $this->resetKillPoint($pln);
+    $vals = $this->checkEg($na);
+    
+    if ($vals){
+      $this->annihilator($na);
+    }
+    else{
+      return true;
+    }
+  }
 }
