dc24b730bf683738dd4881058b061f3a62e71d57
Chen Zheng <czheng1968@gmail.com>
2015-11-25 09:25:29 -0500

    Update Annihilator.php

--- Annihilator/src/Annihilator/Annihilator.php
+++ Annihilator/src/Annihilator/Annihilator.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace Annihilator;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\Listener;
+
+class Annihilator extends PluginBase implements Listener {
+  /*
+  This is a commissioned plugin developed by HotFireyDeath. 
+  License is under the Creative Commons Attribution-ShareAlike 4.0 International.
+  
+  A copy of the license is available in the LICENSE.txt file.
+  */
+  public function onEnable(){
+    if (!is_dir($this->getDataFolder())){
+      mkdir($this->getDataFolder());
+    }
+  }
+  
+}
