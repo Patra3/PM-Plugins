ffd093d25d08040d191985e46019461fac7c5621
Chen Zheng <czheng1968@gmail.com>
2015-11-25 22:16:07 -0500

    not complete still bugged but..
    probably final commit for awhile, busy

--- Annihilator/src/Annihilator/Annihilator.php
+++ Annihilator/src/Annihilator/Annihilator.php
@@ -14,6 +14,7 @@ use pocketmine\event\Listener;
 
 $killer = array();
 $arrowtimeshot = array();
+$bow = new Bow(0, 1);
 
 class Annihilator extends PluginBase implements Listener {
   /*
@@ -29,28 +30,28 @@ class Annihilator extends PluginBase implements Listener {
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
   public function addKillPoint($name){
-    if (!is_file($name.".json")){
+    if (!is_file($this->getDataFolder().$name.".json")){
       $data = array();
       $data["kills"] = 0;
       $data["kills"] = $data["kills"] + 1;
       $encode = json_encode($data);
-      $handle = fopen($name.".json", "w+");
+      $handle = fopen($this->getDataFolder().$name.".json", "w+");
       fwrite($handle, $encode);
       fclose($handle);
     }
     else{
-      $file = file_get_contents($name.".json");
+      $file = file_get_contents($this->getDataFolder().$name.".json");
       $decode = json_decode($file, true);
       $decode["kills"] = $decode["kills"] + 1;
       $encode = json_encode($decode);
-      unlink($name.".json");
-      $handle = fopen($name.".json", "w+");
+      unlink($this->getDataFolder().$name.".json");
+      $handle = fopen($this->getDataFolder().$name.".json", "w+");
       fwrite($handle, $encode);
       fclose($handle);
     }
   }
   public function checkEg($name){
-    $data = file_get_contents($name.".json");
+    $data = file_get_contents($this->getDataFolder().$name.".json");
     $decode = json_decode($data, true);
     if (intval($decode["kills"]) === 7){
       return true;
@@ -60,46 +61,65 @@ class Annihilator extends PluginBase implements Listener {
     }
   }
   public function resetKillPoint($name){
-    if (!is_file($name.".json")){
+    if (!is_file($this->getDataFolder().$name.".json")){
       $data = array();
       $data["kills"] = 0;
       $encode = json_encode($data);
-      $handle = fopen($name.".json", "w+");
+      $handle = fopen($this->getDataFolder().$name.".json", "w+");
       fwrite($handle, $encode);
       fclose($handle);
+      return true;
     }
     else{
       unlink($name.".json");
-      $handle = fopen($name.".json", "w+");
+      $handle = fopen($this->getDataFolder().$name.".json", "w+");
       $data = array();
       $data["kills"] = 0;
       $encode = json_encode($data);
       fwrite($handle, $encode);
       fclose($handle);
+      return true;
     }
   }
   public function annihilator($name){
-    $data = file_get_contents($name.".json");
+    $data = file_get_contents($this->getDataFolder().$name.".json");
     $decode = json_decode($data, true);
     $decode["annihilator"] = "yes";
-    unlink($name.".json");
+    unlink($this->getDataFolder().$name.".json");
     $encode = json_encode($decode);
-    $handle = fopen($name.".json", "w+");
+    $handle = fopen($this->getDataFolder().$name.".json", "w+");
     fwrite($handle, $encode);
     fclose($handle);
     $player = $this->getServer()->getPlayer($name);
-    $bow = new Bow(0, 1);
     $player->getInventory()->addItem($bow);
     $arrowtimeshot[$name] = 3;
     return true;
   }
+  public function detransform($name){
+    $data = file_get_contents($this->getDataFolder().$name.".json");
+    $decode = json_decode($data, true);
+    unset($decode["annihilator"]);
+    $encode = json_encode($decode);
+    unlink($this->getDataFolder().$name.".json");
+    $handle = fopen($this->getDataFolder().$name.".json");
+    fwrite($handle, $encode);
+    fclose($handle);
+    $this->resetKillPoint($name);
+    return true;
+  }
   public function onBowShoot(EntityShootBowEvent $event){
     $player = $event->getEntity()->getName();
-    $data = file_get_contents($player.".json");
+    $ps = $event->getEntity();
+    $data = file_get_contents($this->getDataFolder().$player.".json");
     $decode = json_decode($data, true);
     if ($decode["annihilator"] === "yes"){
       $event->setForce(100);
       $arrowtimeshot[$player] = intval($arrowtimeshot[$player]) - 1;
+      if (intval($arrowtimeshot[$player]) === 0){
+        $ps->getInventory()->removeItem($bow);
+        $this->detransform($player);
+        return true;
+      }
       return true;
     }
   }
@@ -107,7 +127,7 @@ class Annihilator extends PluginBase implements Listener {
     $item = $event->getItem();
     $player = $event->getPlayer();
     if ($item instanceof Bow){
-      $data = file_get_contents($player->getName().".json");
+      $data = file_get_contents($this->getDataFolder().$player->getName().".json");
       $decode = json_decode($data, true);
       if ($decode["annihilator"] != "yes"){
         return true;
@@ -124,10 +144,10 @@ class Annihilator extends PluginBase implements Listener {
       $dam = $event->getDamager();
       if ($dam instanceof Player){
         $ms = $dam->getName();
-        if (!is_file($ms.".json")){
+        if (!is_file($this->getDataFolder().$ms.".json")){
           return true;
         }
-        $hhd = file_get_contents($ms.".json");
+        $hhd = file_get_contents($this->getDataFolder().$ms.".json");
         $dec = json_decode($hhd, true);
         if (isset($decode["annihilator"])){
           if ($decode["annihilator"] === "yes"){
