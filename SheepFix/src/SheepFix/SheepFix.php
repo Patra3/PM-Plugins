d5999c5343fd43510a7a6d62efb439ce5c8e4711
Chen Zheng <czheng1968@gmail.com>
2016-04-23 23:22:04 +0000

    Truly bugfixed SheepFix. Believe me!

--- SheepFix/src/SheepFix/SheepFix.php
+++ SheepFix/src/SheepFix/SheepFix.php
@@ -8,7 +8,7 @@ use pocketmine\utils\TextFormat;
 use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\item\Shears;
+use pocketmine\item\ItemBlock;
 use pocketmine\item\Item;
 use pocketmine\Player;
 use pocketmine\entity\Sheep;
@@ -39,13 +39,14 @@ class SheepFix extends PluginBase implements Listener {
             mkdir($this->getDataFolder());
             $this->saveDefaultConfig();
         }
-        $this->getServer()->getScheduler()->scheduleRepeatingTask(new CooldownTask($this), 10);
+        $this->getServer()->getScheduler()->scheduleRepeatingTask(new CooldownTask($this), 20);
+        $this->setCooldownArray(array());
     }
     public function onItemHeld(PlayerItemHeldEvent $event){
         $item = $event->getItem();
         if ($item->getId() === Item::SHEARS){
             $player = $event->getPlayer();
-            $player->sendMessage(TextFormat::GREEN."Hit a sheep with your shears to shear sheep.");
+            $player->sendPopup(TextFormat::GREEN."Hit a sheep with your shears to shear sheep.");
         }
     }
     public function onDamage(EntityDamageEvent $event){
@@ -53,6 +54,9 @@ class SheepFix extends PluginBase implements Listener {
         $c = $event->getEntity()->getLastDamageCause(); //sheep's last dmg cause
         $sheep = $event->getEntity();
         if ($this->isSheepInCooldown($sheep)){
+            $p = $event->getEntity()->getLastDamageCause()->getDamager(); //player
+            $p->sendPopup(TextFormat::YELLOW."Sheep is in cooldown!");
+            $event->setCancelled();
             return true;
         }
         if ($event->getEntity() instanceof Sheep){
@@ -62,8 +66,10 @@ class SheepFix extends PluginBase implements Listener {
                     $itemf = $d->getInventory()->getItemInHand()->getId();
                     if ($itemf === Item::SHEARS){
                         $event_is = true;
-                        $d->getInventory()->addItem(new ItemBlock(new Wool(), 0, rand(1, 3)));
+                        $amount = rand(1, 3);
+                        $d->getInventory()->addItem(new ItemBlock(new Wool(), 0, $amount));
                         array_push($this->cooldown, new CooldownClass($sheep, $this->getConfig()->get("cooldown")));
+                        $d->sendPopup(TextFormat::GREEN."Sheared and got ".$amount." wool!");
                     }
                 }
             }
@@ -74,5 +80,19 @@ class SheepFix extends PluginBase implements Listener {
     }
     public function setCooldownArray($array){
         $this->cooldown = $array;
+    }
+    private function isSheepInCooldown($sheep){
+        foreach ($this->cooldown as $objs){
+            if ($sheep === $objs->sheep){
+                return true;
+            }
+        }
+        return false;
+    }
+    public function onDisable(){
+        foreach ($this->cooldown as $objs){
+            $objs->sheep->setNameTag("");
+        }
+        $this->getLogger()->info("Sheep cooldowns reset!");
     }
 }
