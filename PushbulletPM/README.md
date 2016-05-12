5041da3d29258db03099c415155b136a152356a2
Chen Zheng <czheng1968@gmail.com>
2016-05-12 23:08:20 +0000

    Added PushbulletPM

---
+++ PushbulletPM/README.md
@@ -0,0 +1,43 @@
+#PushbulletPM  
+Send pushes using Pushbullet, easily!  
+
+### What is Pushbullet?  
+Pushbullet is a 3rd party notifications/messaging program that allows all of your device(s) to be connected.  
+You can deliever notifications to all your devices, and it's a very good service.  
+More info: https://www.pushbullet.com/   
+
+### Well, what does this plugin do?  
+Simply stated, this plugin allows for integration with your Pushbullet account, so you can recieve some basic   
+notifications. I am planning on adding updates to this plugin very soon, with more pushing features!  
+
+### How do I set this up?  
+Watch my video tutorial:  
+https://www.youtube.com/watch?v=JzkbQYnhj-s&feature=youtu.be
+
+### For Developers:  
+Now, I know you are likely anxious to get your hands on this for push notifications.  
+Luckily, I made two (currently) unique methods you can call.  
+
+In the core of my plugin, you can use sendPush(), with the following parameters:  
+token key, title, message, recieving user email  
+
+```
+$pushbulletclass->sendPush($client_api_key, $push_title, $push_message, $recieving_user_email);
+```  
+
+and getPushbulletUser() to check if the user is a registered user:
+token key  
+
+```
+$pushbulletclass->getPushbulletUser($client_api_key);
+```  
+
+Be wary, the second method returns a user object directly from Pushbullet servers.
+
+### What do you plan to add?  
+I plan to add sending global pushes via command, player Pushbullet registration, and  
+more stable features in the future.  
+
+### Why is this plugin connecting to server?  
+The plugin connects to an online server because pushses can't be curl()'d directly  
+from PocketMine servers, and for verification reasons.
---
+++ PushbulletPM/src/PushbulletPM/PushbulletPM.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace PushbulletPM;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Utils;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+
+class PushbulletPM extends PluginBase implements Listener {
+    
+    const VERSION = "1.0.0";
+    public $url; //Server url
+    
+    public function getPushbulletUser($token){
+        /**
+         * Gets the Pushbullet user data. Returns as a Pushbullet User object.
+         * More information: https://docs.pushbullet.com/#get-user
+         * 
+         * @param $token User api token
+         * @return User object
+         * */
+        return Utils::postURL(
+            $this->url,
+            array(
+                "type" => "getPushbulletUser",
+                "data" => json_encode(
+                    array(
+                        "access_token" => $token
+                        )
+                    )
+                )
+        );
+    }
+    public function sendPush($token, $title, $message, $reciever){
+        /**
+         * Sends a push to $reciever via $token's account. $token is the access token.
+         * Please make $reciever the Pushbullet account holder's email.
+         * More information: https://docs.pushbullet.com/#create-push
+         * 
+         * @param $token User api token
+         * @return Push object
+         * */
+        return Utils::postURL(
+            $this->url,
+            array(
+                "type" => "sendPush",
+                "data" => json_encode(
+                    array(
+                        "access_token" => $token,
+                        "title" => $title,
+                        "message" => $message,
+                        "reciever" => $reciever
+                        )
+                    )
+                )
+            );
+    }
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        if (!is_dir($this->getDataFolder())){
+            mkdir($this->getDataFolder());
+            $this->saveDefaultConfig();
+        }
+        elseif (!is_file($this->getDataFolder()."/config.yml")){
+            $this->saveDefaultConfig();
+        }
+        
+        $this->url = "http://fustarbuffet.com/hotfireydeath/projects/pushbulletPM/pushbulletpm.php";
+        
+        $this->getLogger()->info("Verifying connection to PushbulletPM server...");
+        
+        $verify = Utils::getURL($this->url."?type=verify");
+        $verify = json_decode($verify, true);
+        
+        if ($verify["version"] === self::VERSION){
+            $this->getLogger()->info(TextFormat::GREEN."Plugin is up to date!");
+        }
+        else{
+            $this->getLogger()->info(TextFormat::YELLOW."There is a new version of PushbulletPM available...");
+            $this->getLogger()->info(TextFormat::YELLOW."Some errors may occur if you do not update.");
+        }
+        
+        $this->getLogger()->info("Running check to verify configured account...");
+        $data = json_decode($this->getPushbulletUser($this->getConfig()->get("access_token")), true);
+        if (isset($data["active"])){
+            if ($data["active"]){
+                $this->getLogger()->info(TextFormat::GREEN."This account is verified by Pushbullet!");
+                $this->getLogger()->info("Verification credentials: ");
+                $this->getLogger()->info("Name: ".$data["name"]);
+                $this->getLogger()->info("Email: ".$data["email"]);
+            }
+        }
+        elseif ($data["error"]["code"] === "invalid_access_token"){
+            $this->getLogger()->info(TextFormat::RED."Uh oh, that account is invalid. Please check 'access_token'.");
+        }
+        $o = $this->getConfig()->get("server_open")["details"];
+        if ($this->getConfig()->get("server_open")["notify"]){
+            if (!is_array($o["send_to"])){
+                if ($o["send_to"] === "default"){
+                    $this->sendPush($this->getConfig()->get("access_token"), $o["title"], $o["message"], json_decode($this->getPushbulletUser($this->getConfig()->get("access_token")), true)["email"]);
+                }
+                else{
+                    $this->sendPush($this->getConfig()->get("access_token"), $o["title"], $o["message"], $o["send_to"]);
+                }
+            }
+            else{
+                foreach($o["send_to"] as $emails){
+                    $this->sendPush($this->getConfig()->get("access_token"), $o["title"], $o["message"], $emails);
+                }
+            }
+            $this->getLogger()->info(TextFormat::GREEN."Server notifications sent to Pushbullet!");
+        }
+    }
+    public function onDisable(){
+        $o = $this->getConfig()->get("server_close")["details"];
+        if ($this->getConfig()->get("server_close")["notify"]){
+            if (!is_array($o["send_to"])){
+                if ($o["send_to"] === "default"){
+                    $this->sendPush($this->getConfig()->get("access_token"), $o["title"], $o["message"], json_decode($this->getPushbulletUser($this->getConfig()->get("access_token")), true)["email"]);
+                }
+                else{
+                    $this->sendPush($this->getConfig()->get("access_token"), $o["title"], $o["message"], $o["send_to"]);
+                }
+            }
+            else{
+                foreach($o["send_to"] as $emails){
+                    $this->sendPush($this->getConfig()->get("access_token"), $o["title"], $o["message"], $emails);
+                }
+            }
+            $this->getLogger()->info(TextFormat::GREEN."Server notifications sent to Pushbullet!");
+        }
+    }
+    public function onJoin(PlayerJoinEvent $event){
+        $o = $this->getConfig()->get("player_join");
+        if ($o["notify"]){
+            if (!is_array($o["player_notifications"])){
+                $this->getLogger()->info(TextFormat::YELLOW."Player notifications not detected as list.");
+            }
+            else{
+                foreach($o["player_notifications"] as $name){
+                    if ($event->getPlayer()->getName() === $name){
+                        $o["title"] = $event->getPlayer()->getName()." came online!";
+                        $o["message"] = $event->getPlayer()->getName()." came onto the server!";
+                        if (!is_array($o["send_to"])){
+                            if ($o["send_to"] === "default"){
+                                $this->sendPush($this->getConfig()->get("access_token"), $o["title"], $o["message"], json_decode($this->getPushbulletUser($this->getConfig()->get("access_token")), true)["email"]);
+                            }
+                            else{
+                                $this->sendPush($this->getConfig()->get("access_token"), $o["title"], $o["message"], $o["send_to"]);
+                            }
+                        }
+                        else{
+                            foreach($o["send_to"] as $emails){
+                                $this->sendPush($this->getConfig()->get("access_token"), $o["title"], $o["message"], $emails);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public function onQuit(PlayerQuitEvent $event){
+        $o = $this->getConfig()->get("player_disconnects");
+        if ($o["notify"]){
+            if (!is_array($o["player_notifications"])){
+                $this->getLogger()->info(TextFormat::YELLOW."Player notifications not detected as list.");
+            }
+            else{
+                foreach($o["player_notifications"] as $name){
+                    if ($event->getPlayer()->getName() === $name){
+                        $o["title"] = $event->getPlayer()->getName()." went offline!";
+                        $o["message"] = $event->getPlayer()->getName()." went off the server.";
+                        if (!is_array($o["send_to"])){
+                            if ($o["send_to"] === "default"){
+                                $this->sendPush($this->getConfig()->get("access_token"), $o["title"], $o["message"], json_decode($this->getPushbulletUser($this->getConfig()->get("access_token")), true)["email"]);
+                            }
+                            else{
+                                $this->sendPush($this->getConfig()->get("access_token"), $o["title"], $o["message"], $o["send_to"]);
+                            }
+                        }
+                        else{
+                            foreach($o["send_to"] as $emails){
+                                $this->sendPush($this->getConfig()->get("access_token"), $o["title"], $o["message"], $emails);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
